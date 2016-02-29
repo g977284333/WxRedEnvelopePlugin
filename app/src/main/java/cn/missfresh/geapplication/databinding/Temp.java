d43297538af1f90846236756cf6cc5da85f3858c
gechen <g_chen1206@163.com>
2016-02-29 12:24:30 +0800

    delete unUsed Code

--- app/src/main/java/cn/missfresh/geapplication/databinding/Temp.java
+++
@@ -1,7 +0,0 @@
-package cn.missfresh.geapplication.databinding;
-
-/**
- * Created by gchen on 16/2/25.
- */
-public class Temp {
-}
--- app/src/main/java/cn/missfresh/geapplication/domain/User.java
+++
@@ -1,9 +0,0 @@
-package cn.missfresh.geapplication.domain;
-
-/**
- * Created by gchen on 16/2/25.
- */
-public class User {
-    public String firstName;
-    public String lastName;
-}
